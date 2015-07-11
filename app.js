a3126cefe43da7f5bf502b2805aec3613d036424
Alexander Handler <Alexjhandler@gmail.com>
2015-07-11 13:38:42 -0400

    inital commit, basic call down

---
+++ app.js
@@ -0,0 +1,29 @@
+var http =  require("http");
+
+function printMessage(info){
+	console.log(info)
+}
+
+function printError(error){
+	console.log(error.message)
+}
+
+var req = http.get("http://swapi.co/api/species/"+ Math.floor((Math.random() * 10) + 1)+"/", function(res){
+	var body = " ";
+
+	res.on('data', function (chunk){
+	body += chunk;
+	});
+	res.on('end',function(){
+		if(res.statusCode === 200){
+			try{
+				var result = JSON.parse(body);
+				printMessage(result)
+			} catch(error){
+				printError(error)
+			}
+		} else {
+			printError({message: "there was no info for that person!"})
+		}
+	})
+});
