580ccf551cd47cbdb8de922034fb7513753251be
Alexander Handler <Alexjhandler@gmail.com>
2015-07-14 23:18:36 -0400

    make vars

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ function printMessage(info){
 function printError(error){
 	console.log(error.message)
 }
-
+randomNumber = Math.floor((Math.random() * 10) + 1)
 userInput = process.argv[2];
 
-var req = http.get("http://swapi.co/api/"+ userInput + "/"+ Math.floor((Math.random() * 10) + 1)+"/", function(res){
+var req = http.get("http://swapi.co/api/"+ userInput + "/"+ randomNumber +"/", function(res){
 	var body = " ";
 
 	res.on('data', function (chunk){
